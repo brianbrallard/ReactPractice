f25ad37c74910805dd7bd6e499cee15895287ed0
brianbrallard <brianbrallard2799@gmail.com>
2023-11-29 03:03:38 -0300

    Add initial files for React project

---
+++ projects/firstproject/src/App.jsx
@@ -0,0 +1,18 @@
+export function App(){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/brianbrallard" alt="avatar de Brian" />
+                <div>
+                    <strong>Brian Polo</strong>
+                    <span>@brianbrallard</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
