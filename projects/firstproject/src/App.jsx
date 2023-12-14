eadfd1e3589d692ded9377c1472c6b0e5b460dea
brianbrallard <brianbrallard2799@gmail.com>
2023-12-14 03:07:15 -0300

    Refactor TwitterFollowCard component in App.jsx

--- projects/firstproject/src/App.jsx
+++ projects/firstproject/src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App(){
-    const [isFollowing, setIsFollowing] = useState(false)
 
     return (
         <section className='App'>
-        <TwitterFollowCard userName='brianbrallard' initialIsFollowing={true}>
+        <TwitterFollowCard userName='brianbrallard'>
             Brian Polo
             </TwitterFollowCard>
-        <TwitterFollowCard userName="brianbrallard" >
+        <TwitterFollowCard userName='brianbrallard'>
             Brian Polo
-            </TwitterFollowCard>
-
-        <button onClick={()=> setIsFollowing(!isFollowing)}>Cambiar estado de app</button>    
-        <TwitterFollowCard isFollowing userName="brianbrallard" name="Brian Polo"/>
-        <TwitterFollowCard isFollowing userName="brianbrallard" name="Brian Polo"/>
+        </TwitterFollowCard>
        
         </section>
         
