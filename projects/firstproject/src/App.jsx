987b1405e337bd7279952bd3259e7c12ac411ff7
brianbrallard <brianbrallard2799@gmail.com>
2023-12-14 03:44:27 -0300

    Refactor TwitterFollowCard component and update App component

--- projects/firstproject/src/App.jsx
+++ projects/firstproject/src/App.jsx
@@ -2,14 +2,47 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App(){
 
+const users = [
+    {
+        userName: 'brianbrallard',
+        name: 'Brian Polo', 
+        isFollowing: true
+    },
+    {
+        userName: 'brianbrallard', 
+        name: 'Brian Polo', 
+        isFollowing: true
+    },
+    {
+        userName: 'brianbrallard', 
+        name: 'Brian Polo', 
+        isFollowing: false
+    },
+    {
+        userName: 'brianbrallard', 
+        name: 'Brian Polo', 
+        isFollowing: false
+    }
+    
+]
+
+
     return (
         <section className='App'>
-        <TwitterFollowCard userName='brianbrallard'>
-            Brian Polo
-            </TwitterFollowCard>
-        <TwitterFollowCard userName='brianbrallard'>
-            Brian Polo
-        </TwitterFollowCard>
+            {
+                users.map(({ userName, name, isFollowing })=>(
+                
+
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                            >
+                             {name}
+                            </TwitterFollowCard>
+
+                ))
+            }
        
         </section>
         
